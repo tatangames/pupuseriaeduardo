77f63c6c538b920b483d03a3341ef7e046b962b5
giovanyrosales <gioer21@gmail.com>
2022-05-27 20:30:28 -0600

    ajuste de notificacion 3

--- app/Console/Commands/EnvioNotiOrdenesCrob.php
+++ app/Console/Commands/EnvioNotiOrdenesCrob.php
@@ -2,9 +2,12 @@
 
 namespace App\Console\Commands;
 
+use App\Jobs\SendNotiMotoristaJobs;
 use App\Jobs\SendNotiPropietarioJobs;
 use App\Models\Afiliados;
 use App\Models\InformacionAdmin;
+use App\Models\Motoristas;
+use App\Models\MotoristasOrdenes;
 use App\Models\Ordenes;
 use Carbon\Carbon;
 use Illuminate\Console\Command;
@@ -23,7 +26,7 @@ class EnvioNotiOrdenesCrob extends Command
      *
      * @var string
      */
-    protected $description = 'Envio Noti cada minuto a ordenes nuevas a los propietarios, sino ha contestado aun';
+    protected $description = 'Noti cada minuto a ordenes nuevas a los propietarios o motorista, sino ha contestado aun';
 
     /**
      * Create a new command instance.
@@ -72,6 +75,59 @@ class EnvioNotiOrdenesCrob extends Command
                     SendNotiPropietarioJobs::dispatch($titulo, $mensaje, $pilaPropietarios);
                 }
             }
+
+            // notificacion a motoristas que hay ordenes nuevas para que las agarren
+            $ordenMotorista = Ordenes::where('estado_2', 1) // inicio preparacion
+            ->where('estado_7', 0) // no ha sido cancelada
+            ->where('tipoentrega', 1) // domicilio
+            ->whereDate('fecha_orden', '=', Carbon::today('America/El_Salvador')->toDateString())
+                ->count();
+
+            $seguroNotiMotorista = false;
+
+            if($ordenMotorista > 0){
+
+                $ordenFore = Ordenes::where('estado_2', 1) // inicio preparacion
+                ->where('estado_7', 0) // no ha sido cancelada
+                ->where('tipoentrega', 1) // domicilio
+                ->whereDate('fecha_orden', '=', Carbon::today('America/El_Salvador')->toDateString())
+                    ->get();
+
+                // hoy verificar sino han agarrado esa orden
+                foreach ($ordenFore as $ff){
+
+                    if(MotoristasOrdenes::where('ordenes_id', $ff->id)->first()){
+                        // orden agarrada
+                    }else{
+                        // falta orden sin agarrar, enviar notificacion
+                        $seguroNotiMotorista = true;
+                        break;
+                    }
+                }
+
+                if($seguroNotiMotorista){
+
+                    // notificacion a todos los motoristas que hay orden por agarrar
+                    $listaMotoristas = Motoristas::where('activo', 1)
+                        ->where('disponible', 1)
+                        ->get();
+
+                    $pilaMotoristas = array();
+                    foreach($listaMotoristas as $p){
+                        if($p->token_fcm != null){
+                            array_push($pilaMotoristas, $p->token_fcm);
+                        }
+                    }
+
+                    $titulo = "Hay Nuevas Ordenes";
+                    $mensaje = "Por Favor Verificar";
+
+                    if($pilaMotoristas != null) {
+                        SendNotiMotoristaJobs::dispatch($titulo, $mensaje, $pilaMotoristas);
+                    }
+                }
+            }
+
         }
     }
 }
